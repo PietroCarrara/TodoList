3b0a97b12e1ed3be418a0d5942c66512ad71aaf4
Pietro Carrara <piticarrara@gmail.com>
2019-02-25 15:07:27 -0300

    Remove default laravel files

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+</head>
+
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+    @yield('scripts')
+</body>
+
+</html>
