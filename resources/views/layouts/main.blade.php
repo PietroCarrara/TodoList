9ccef45fa56d3580654a108c60974be89fb25f07
Pietro Carrara <piticarrara@gmail.com>
2019-02-26 15:41:16 -0300

    Display errors as toasts

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,6 +9,20 @@
     <div class="container">
         @yield('content')
     </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    {{-- Display errors as toasts --}}
+    @if ($errors->any())
+        <script>
+            var errors = @json($errors->all());
+
+            for (var err of errors) {
+                M.toast({
+                    html: err,
+                    displayDuration: 4000,
+                });
+            }
+        </script>
+    @endif
     @yield('scripts')
 </body>
 
